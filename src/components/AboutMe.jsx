705c8d8f1ca43697d0ddcf68f4e197c219191e72
Pradnya Joshi <psjoshi20@Shrikant-iMac.local>
2019-09-29 17:54:01 +0800

    Formatted Aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -13,17 +13,17 @@ const AboutMe = (props) => {
                 </Grid>
                 <Grid item xs={9}>
                         <Typography component="div" className={props.style1.para1} >
-                        Hello, I am Pradnya Joshi. 
-                        I am a Homemaker.My daily routine revolves around my family activities .I have an educational background in computer programming and fundamentals. Recently, I have started to learn again and refresh my knowledge in software Technologies in my free time. 
-                        I am actively looking ways to explore opportunities to learn and grow my knowledge. 
+                        <p>Hello, I am Pradnya Joshi. </p>
+                        <p>I am a Homemaker.My daily routine revolves around my family activities .I have an educational background in computer programming and fundamentals. Recently, I have started to learn again and refresh my knowledge in software Technologies in my free time. </p>
+                        <p> I am actively looking ways to explore opportunities to learn and grow my knowledge. </p>
 
-                        I first came upon TechLadies website in my some search related web development . That piqued my curiosity to find out more on the TechLadies website. My takeaway impression from the site was that it was a great initiative that brings people together to explore their interest in technology and encourage sharing of their knowledge.
+                        <p> I first came upon TechLadies website in my some search related web development . That piqued my curiosity to find out more on the TechLadies website. My takeaway impression from the site was that it was a great initiative that brings people together to explore their interest in technology and encourage sharing of their knowledge.</p>
 
-                        I was looking for an avenue to practice and learn web development skills . I searched may skill development courses offered in mySkillfuture. I chanced upon a videos on the TechLadies Bootcamps. The projects shared in the video looked interesting, and I liked how the programme provided the opportunity to learn directly from coaches and work with a group of fellow peers on a project.
+                        <p> I was looking for an avenue to practice and learn web development skills . I searched may skill development courses offered in mySkillfuture. I chanced upon a videos on the TechLadies Bootcamps. The projects shared in the video looked interesting, and I liked how the programme provided the opportunity to learn directly from coaches and work with a group of fellow peers on a project.</p>
 
-                        I am really keen on getting into the programme I had spent quite a bit of time researching and watching YouTube videos to do up an application that is required as part of the bootcamp application..
+                        <p> I am really keen on getting into the programme I had spent quite a bit of time researching and watching YouTube videos to do up an application that is required as part of the bootcamp application..</p>
 
-                        I want to work on an app for an actual company with a real problem, I believe that the bootcamp can help me in Practical experience working on an actual app. I need a A structured program with clear objectives and an achievable goal and also I believe good coach and mentors with actual industry knowledge will help me through this.
+                        <p> I want to work on an app for an actual company with a real problem, I believe that the bootcamp can help me in Practical experience working on an actual app. I need a A structured program with clear objectives and an achievable goal and also I believe good coach and mentors with actual industry knowledge will help me through this.</p>
                         </Typography>
                 </Grid>
               </Grid>;
